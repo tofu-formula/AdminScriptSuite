2acbf7cd940c8930a9f966e02ad4d79381ee50d9
tofu-formula <62560384+tofu-formula@users.noreply.github.com>
2025-10-23 17:31:15 -0700

    changing execution policy to bypass

--- Installers/Install-DellCommandUpdate-FullClean_RUNNER.bat
+++ Installers/Install-DellCommandUpdate-FullClean_RUNNER.bat
@@ -1,2 +1,2 @@
 @echo off
-Powershell.exe -executionpolicy remotesigned -File "%~dp0Install-DellCommandUpdate-FullClean.ps1"
+Powershell.exe -executionpolicy bypass -File "%~dp0Install-DellCommandUpdate-FullClean.ps1"
