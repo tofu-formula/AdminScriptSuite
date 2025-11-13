f1b92a3c2082c58eff2c14f891ec700eadd5efc0
tofu-formula <62560384+tofu-formula@users.noreply.github.com>
2025-11-13 13:47:00 -0800

    Renaming

--- Installers/Install-DellCommandUpdate-FullClean_RUNNER.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-Powershell.exe -executionpolicy bypass -File "%~dp0Install-DellCommandUpdate-FullClean.ps1"
-Pause
---
+++ Installers/InstallApp-DellCommandUpdate-FullClean_RUNNER.bat
@@ -0,0 +1,3 @@
+@echo off
+Powershell.exe -executionpolicy bypass -File "%~dp0InstallApp-DellCommandUpdate-FullClean.ps1"
+Pause
